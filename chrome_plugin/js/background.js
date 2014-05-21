2d4a0eae26736948b27a4a5c8879b264ccfe26a7
zhangyiyang <yiyangzhang@ctrip.com>
2014-05-21 14:15:35 +0800

    change

--- chrome_plugin/js/background.js
+++ chrome_plugin/js/background.js
@@ -1,13 +1,8 @@
 // 可设置 browserAction 中 icon 图片
-chrome.browserAction.setIcon({path:"../images/icon.png"});
-
+//chrome.browserAction.setIcon({path:"../images/icon.png"});
+function updateIcon(){
+  chrome.browserAction.setIcon({path:"../images/icon.png"});
+}
 
 // React when a browser action's icon is clicked.
-chrome.browserAction.onClicked.addListener(function(tab) {
-      chrome.tabs.create();
-});
-
-
-function ba(){
- alert(789);
-}
+chrome.browserAction.onClicked.addListener(updateIcon);
