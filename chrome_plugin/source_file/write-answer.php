1f88d3f0d64a76399fd06ec21b2125917db9b101
zhangyiyang <yiyangzhang@ctrip.com>
2014-09-30 17:16:38 +0800

    fill answer ,get mothod

--- chrome_plugin/source_file/write-answer.php
+++ chrome_plugin/source_file/write-answer.php
@@ -6,18 +6,32 @@
 </head>
 <body>
 <?php
+$filePath = "answer.json";
 if(isset($_GET["data"])){
 	$data = $_GET["data"];
+	$file = fopen($filePath,"w");
+	echo fwrite($file,$data);
+	fclose($file);
 }
 
-$file = fopen("answer.json","w");
-echo fwrite($file,$data);
-fclose($file);
 //转换为json对象
 //json_decode($data);
 
 
-echo"<script>window.close();</script>";
+// delete answer.js file
+if(isset($_GET["deletefile"]) && $_GET["deletefile"] == true){
+	if (file_exists($filePath)){
+		if(unlink($filePath)){
+			echo("Deleted $filePath");
+		}else{
+			echo("Error deleting $filePath");
+		}
+	}
+}
+
+if(!isset($_GET["close"])){
+	echo"<script>window.close();</script>";
+}
 ?>
 </body>
 </html>
