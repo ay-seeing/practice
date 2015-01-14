9a8ae48b576f7ee36396ec8713c174b914c2bf0c
yiyangzhang <yiyangzhang@Ctrip.com>
2015-01-14 14:49:16 +0800

    xml2js插件测试项目文件修改

--- node/xmlParser/app.js
+++ node/xmlParser/app.js
@@ -1,16 +1,19 @@
 // 解析xml数据字符串
-/*var parseString = require('xml2js').parseString;
+var parseString = require('xml2js').parseString;
 var xml = '<?xml version="1.0" ?> <customer> <field id="firstName"> <value>David</value> </field> <field id="lastName"> <value>Smith</value> </field> <field id="country"> <value>China</value> </field> </customer>';
 parseString(xml ,function(err,result){
 	console.log(result);
 	// 将result以字符串输出，而不现实project
 	// console.log(util.inspect(result,false,null));
+
+	var str = JSON.stringify(result);
+	console.log(str);
 });
-*/
+
 
 
 // 解析xml数据文件
-var fs = require('fs'),
+/*var fs = require('fs'),
 	util = require('util'),
 	xml2js = require('xml2js');
 var parser = new xml2js.Parser();
@@ -24,4 +27,4 @@ fs.readFile(__dirname + '/customer.xml', function(err, data){
 		// console.log(str);
 		console.log('Done');
 	})
-})
+})*/
